ba3188209ee245ecc42f7eb2963ebc9d754cd134
bootstraponline <code@bootstraponline.com>
2015-11-09 14:25:19 -0500

    Release 0.0.3

--- lib/rspec_expectation_count/version.rb
+++ lib/rspec_expectation_count/version.rb
@@ -1,4 +1,4 @@
 module RSpecExpectationCount
-  VERSION = '0.0.2' unless defined? ::RSpecExpectationCount::VERSION
+  VERSION = '0.0.3' unless defined? ::RSpecExpectationCount::VERSION
   DATE    = '2015-11-09' unless defined? ::RSpecExpectationCount::DATE
 end
